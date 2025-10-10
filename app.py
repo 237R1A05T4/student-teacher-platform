b2114d9535445f0d093890bf769e5cb43f974b75
237R1A05T4 <237r1a05t4@cmrtc.ac.in>
2025-10-10 19:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,6 @@ def post_announcement():
     return jsonify({"error": "Invalid data"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
